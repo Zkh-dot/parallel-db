606bad8139c7a92e88b8aa4be19d61e77395bd1e
Zkh-dot <kernzahar@gmail.com>
2023-12-24 15:03:03 +0300

    basic methods in connection class

--- db_connection/connection.py
+++ db_connection/connection.py
@@ -8,10 +8,144 @@ import pandas as pd
 import sqlalchemy
 import time
 from datetime import timedelta
-from core.time_predictor.time_predictor import TimePredictor
+from time_predictor.time_predictor import TimePredictor
+from typing import Union
+from inspect import isclass
 
 # класс для создания подключений к базам данных
+class new_connection:
+    predictor = TimePredictor
+    def __init__(self, logger: logging.Logger = None, df_connection: Union(pyodbc.Connection, cx.connect) = None, login: str = None, password: str = None) -> None:
+        # self.
+        self.__login = login
+        self.__password = password
+        self.__logger = logger
+        self.__connection = None
+        self.__cursor = None
+        self.__predictor = TimePredictor(logger)
+        self.connection = df_connection
+    
+    @property
+    def login(self):
+        return self.__login
+    
+    @login.setter
+    def login(self, login: str):
+        self.__login = login
+        
+    @property
+    def password(self): 
+        return "*" * len(self.__password)
+    
+    @password.setter
+    def password(self, value):
+        self.__password = value
+        
+    @property
+    def cursor(self):
+        return self.__cursor
+    
+    @cursor.setter
+    def cursor(self, value):
+        self.__cursor = value
+    
+    @cursor.deleter
+    def cursor(self):
+        try:
+            self.__cursor.close()
+        except (pyodbc.ProgrammingError, cx.InterfaceError) as error:
+            self.__logger.warning("cursor already closed (•-•)⌐")
+        except AttributeError:
+            self.__logger.error('cursor does not exist')
+        
+    @property
+    def connection(self):
+        return self.__connection    
+        
+    def __connect_class(self, db_connection: Union(pyodbc.Connection, cx.connect)):
+        try:
+            self.__connection = db_connection(self.__login, self.__password)
+            self.__cursor = self.__connection.cursor()
+        except Exception as e:
+            self.__logger.error(e)
+            raise e
+            
+    def __connect_instance(self, db_connection: Union(pyodbc.Connection, cx.connect)):
+        try:
+            self.__connection = db_connection
+            self.__cursor = self.__connection.cursor()
+        except Exception as e:
+            self.__logger.error(e)
+            raise e 
+        
+    def __connect_engine(self, db_connection: sqlalchemy.Engine):
+        try:
+            self.__connection = db_connection.connect()
+            self.__cursor = self.__connection
+        except Exception as e:
+            self.__logger.error(e)
+            raise e
+    
+    @connection.setter
+    def __connect(self, db_connection: Union(pyodbc.Connection, cx.connect, sqlalchemy.Engine)):
+        if db_connection is None:
+            self.__logger.debug("db_connection is None")
+        if isinstance(db_connection, sqlalchemy.Engine):
+            self.__connect_engine(db_connection)
+        elif isclass(db_connection):
+            self.__connect_class(db_connection)
+        else:
+            self.__connect_instance(db_connection)
+            
+    @connection.deleter
+    def connection(self):
+        try:
+            self.__connection.close()
+        except (pyodbc.ProgrammingError, cx.InterfaceError) as error:
+            self.__logger.error("connection already closed \\(°Ω°)/")
+        except AttributeError:
+            self.__logger.error('connection does not exist')
+            
+    def __copy__(self):
+        """
+        returns copy of obj
+        """
+        new = new_connection(self.__logger, None, self.__login, self.__password)
+        new.connection = self.connection
+        new.__cursor = copy.deepcopy(self.__cursor)
+        return new
+    
+    def new_cursor(self):
+        """
+        Returns connection obj with new cursor
+        """
+        new = self.__copy__()
+        new.__cursor = new.__connection.cursor()
+        return new
+    
+    def __disconnect(self):
+        """
+        Disconnects from the database
+        """
+        del self.cursor
+        del self.connection
 
+    def __del__(self):
+        """
+        Destructor that disconnects from the database.
+        """
+        self.__logger.debug("saving history...")
+        self.__predictor.save()
+        self.__disconnect()
+        
+    def commit(self, really_try=True):
+        try:
+            self.__cursor.commit()
+            return True
+        except Exception as e:
+            if really_try:
+                self.__logger.error(e)
+            return False
 
 class connecion:
     predictor = TimePredictor
