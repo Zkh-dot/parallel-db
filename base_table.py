00be91a6e494e7c383554388ff80571eff3110f7
Zkh-dot <kernzahar@gmail.com>
2023-12-25 11:20:46 +0300

    base_table interface

--- base_table.py
+++ base_table.py
@@ -1,24 +1,43 @@
-from core.db_connection.connection import connecion
+from db_connection.connection import connection
 import os
 import threading
+import functools
 from rich.progress import Progress
+import logger
+from db_connection.connecion_factory import connecion_factory
+from logging import Logger
+import decorators.utils as utils
 
 class base_table():
-    connecion = None
     table = None
+    connection_name = str
     requirements = []
+    stages = []
 
-    def __init__(self, logger, conn: connecion):
-        self.logger = logger
-        if isinstance(conn, connecion):
-            self.conn = conn
+    def __init__(self, __logger: Logger = None, db_connection: connection = None, connection_factory: connecion_factory = None):
+        if __logger == None:
+            self.__create_logger()
         else:
-            print("not supported connection!")
-            raise
-        self.stages = []
-
-    @staticmethod
-    def sql_scripts_path(file):
+            self.__logger = __logger
+            
+        if isinstance(db_connection, connection):
+            self.connection = connection
+        else:
+            raise TypeError("db_connection is not connection")
+        for i, table in enumerate(self.requirements):
+            self.requirements[i] = connection_factory.connect_table(__logger, table)
+            
+    def __init_subclass__(cls):
+        cls.__set_sql_scripts_path()
+        
+    def __create_logger(self):
+        self.__logger = logger.get_logger(self.__class__.__name__)
+        decorator_with_logger = functools.partial(logger.trace_call, self.__logger)
+        utils.decorate_function_by_name(decorator_with_logger, "read_sql", "pandas")
+        utils.decorate_function_by_name(decorator_with_logger, "Cursor.execute", "pyodbc")
+        utils.decorate_function_by_name(decorator_with_logger, "DataFrame.to_sql", "pandas")
+            
+    def __set_sql_scripts_path(self):
         """
         Returns the path to the directory containing SQL scripts.
 
@@ -28,7 +47,7 @@ class base_table():
         Returns:
             str: The path to the directory containing SQL scripts.
         """
-        return os.path.join(os.path.dirname(os.path.abspath(file)), "sql_scripts")
+        self.__sql_path =  os.path.join(os.path.dirname(os.path.abspath(__file__)), "sql_scripts")
 
     def command(self, script_name: str) -> str:
         """
@@ -40,7 +59,7 @@ class base_table():
         Returns:
             str: The content of the SQL script.
         """
-        with open(os.path.join(self.sql_scripts, script_name), "r", encoding="utf-8") as sql_script:
+        with open(os.path.join(self.__sql_path, script_name), "r", encoding="utf-8") as sql_script:
             return sql_script.read()
 
     def build(self):
@@ -68,10 +87,10 @@ class base_table():
             for i, r in enumerate(self.requirements):
                 x = threading.Thread(target=r.build_paral, args=(progress,))
                 threads.append(x)
-                self.logger.info(f"Start thread for {r.__class__.__name__} ( ˶ˆ꒳ˆ˵ )")
+                self.__logger.info(f"Start thread for {r.__class__.__name__} ( ˶ˆ꒳ˆ˵ )")
                 x.start()
             for index, thread in enumerate(threads):
-                self.logger.info(f"successfully calculated {self.requirements[index].__class__.__name__} ˶ᵔ ᵕ ᵔ˶")
+                self.__logger.info(f"successfully calculated {self.requirements[index].__class__.__name__} ˶ᵔ ᵕ ᵔ˶")
                 thread.join()
             for stage in self.stages:
                 progress.update(self.task, advance=1)
---
+++ db_connection/connecion_factory.py
@@ -0,0 +1,26 @@
+from .connection import connection
+from base_table import base_table
+from logging import Logger
+
+
+class connecion_factory:
+    def __init__(self, connections: dict[str, connection]):
+        self.connections = connections
+
+    def connect_table(self, logger: Logger,  table: base_table):
+        if not table.connection_name:
+            logger.error(
+                "Not declared connection in table {}!".format(table.__name__))
+        try:
+            table = table(logger, self.connections[table.connection_name.lower()], self)
+        except KeyError:
+            if connection == None:
+                logger.error(
+                    "No connection in table {}!".format(table.__name__))
+            else:
+                logger.error(
+                    "Not supported connection in table {}: {}!".format(table.__name__, table.connection_name))
+        except Exception:
+            logger.error("something went wrong")
+            raise
+        return table
